989ca70862d7b20998874be0792ac329cab4555c
iliags <19849002+iliags@users.noreply.github.com>
2024-08-24 05:04:47 -0700

    Update repeat.rs

--- src/settings/repeat.rs
+++ src/settings/repeat.rs
@@ -3,7 +3,7 @@ use strum_macros::EnumIter;
 #[derive(Debug, Default, Clone, serde::Deserialize, serde::Serialize, PartialEq, EnumIter)]
 pub enum RepeatType {
     Repeat(usize),
-    #[default]
     RepeatUntilStopped,
+    #[default]
     Turbo,
 }
