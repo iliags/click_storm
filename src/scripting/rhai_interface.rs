7e1791c63e7648c9799d831e20b18c3cdc7fa6f9
iliags <19849002+iliags@users.noreply.github.com>
2024-08-27 22:02:32 -0700

    Update rhai_interface.rs

--- src/scripting/rhai_interface.rs
+++ src/scripting/rhai_interface.rs
@@ -3,6 +3,7 @@ use std::sync::{Arc, Mutex};
 
 use device_query::DeviceState;
 use enigo::{Button, Enigo, Mouse, Settings};
+use rand::Rng;
 use rhai::{exported_module, Engine};
 
 use crate::input::mouse_button::{MouseButton, MouseButtonModule};
@@ -14,6 +15,7 @@ pub struct RhaiInterface {
     engine: Arc<Mutex<Engine>>,
     enigo: Arc<Mutex<Enigo>>,
     device: DeviceState,
+    rng: rand::rngs::ThreadRng,
 }
 
 impl RhaiInterface {
@@ -26,6 +28,7 @@ impl RhaiInterface {
             engine: Arc::new(Mutex::new(Engine::new())),
             enigo: Arc::new(Mutex::new(enigo)),
             device: DeviceState::new(),
+            rng: rand::thread_rng(),
         }
     }
 
@@ -83,25 +86,45 @@ impl RhaiInterface {
 
         // Register the functions
         engine.register_fn("new_engine", RhaiInterface::new);
-        engine.register_fn("click_at", RhaiInterface::click_at);
 
         // TODO: Add duration/lerp versions
-        engine.register_fn("move_mouse", RhaiInterface::move_mouse_to);
-        engine.register_fn("add_position", RhaiInterface::add_position);
-        engine.register_fn("drag_to", RhaiInterface::drag_to);
-        engine.register_fn("drag_from_to", RhaiInterface::drag_from_to);
+        engine
+            .register_fn("click_at", RhaiInterface::click_at)
+            .register_fn("move_mouse", RhaiInterface::move_mouse_to)
+            .register_fn("add_position", RhaiInterface::add_position)
+            .register_fn("drag_to", RhaiInterface::drag_to)
+            .register_fn("drag_from_to", RhaiInterface::drag_from_to);
 
         engine.register_fn("get_screen_size", RhaiInterface::get_screen_size);
+
+        engine
+            .register_fn("rand_range", RhaiInterface::rand_range)
+            .register_fn("rand_range_excl", RhaiInterface::rand_range_excl)
+            .register_fn("rand_bool", RhaiInterface::rand_bool);
     }
 
     /**************************************************************************
      * Scripting API
      **************************************************************************/
+    // Note: I am pretty sure calling an API function from another API function will cause issues
 
     /// Click at the specified coordinates with the specified mouse button.
     fn click_at(&mut self, x: i32, y: i32, mouse_button: MouseButton) {
         let mut enigo = self.enigo.lock().unwrap();
         let _ = enigo.move_mouse(x, y, enigo::Coordinate::Abs);
+        let _ = enigo.button(mouse_button.into(), enigo::Direction::Click);
+    }
+
+    // Click within the specified specified area, X/Y starts at the top left corner.
+    fn click_within(&mut self, x: i32, y: i32, width: i32, height: i32, mouse_button: MouseButton) {
+        let mut enigo = self.enigo.lock().unwrap();
+
+        let rand_coords = (
+            self.rng.gen_range(x..=x + width),
+            self.rng.gen_range(y..=y + height),
+        );
+
+        let _ = enigo.move_mouse(rand_coords.0, rand_coords.1, enigo::Coordinate::Abs);
         let _ = enigo.button(mouse_button.into(), enigo::Direction::Click);
     }
 
@@ -145,4 +168,23 @@ impl RhaiInterface {
 
         screen_size.into()
     }
+
+    /// Get a random number within the specified range (min inclusive, max inclusive).
+    fn rand_range(&mut self, min: i32, max: i32) -> i32 {
+        self.rng.gen_range(min..=max)
+    }
+
+    /// Get a random number within the specified range (min inclusive, max exclusive).
+    fn rand_range_excl(&mut self, min: i32, max: i32) -> i32 {
+        self.rng.gen_range(min..max)
+    }
+
+    /// Get a random boolean value (50/50).
+    fn rand_bool(&mut self) -> bool {
+        self.rng.gen_bool(0.5)
+    }
+
+    /**************************************************************************
+     * End of Scripting API
+     **************************************************************************/
 }
