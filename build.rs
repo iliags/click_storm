38a7507955dfd06d088d385ee02338126650aef8
iliags <19849002+iliags@users.noreply.github.com>
2024-08-29 02:23:03 -0700

    Re-enable build script and fix scripting feature

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 //! This file is used to compile the resources for the Windows executable.
 
 fn main() {
-    /*
+    
     extern crate embed_resource;
     use std::env;
     let target = env::var("TARGET").unwrap();
@@ -9,5 +9,5 @@ fn main() {
         // Set an icon for the executable
         embed_resource::compile("assets/icon.rc", embed_resource::NONE);
     }
-     */
+     
 }
