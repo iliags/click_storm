21347bd2e391658f1b029753337900625c22385c
iliags <19849002+iliags@users.noreply.github.com>
2025-02-12 02:33:05 -0700

    Added double click benchmark
    Rename benchmark labels

--- benches/worker_benchmark.rs
+++ benches/worker_benchmark.rs
@@ -17,7 +17,20 @@ pub fn criterion_benchmark(c: &mut Criterion) {
 
     let settings = settings;
 
-    c.bench_function("worker", |b| {
+    c.bench_function("single click, 10 times, 16ms", |b| {
+        b.iter(|| worker_thread(black_box(settings.clone()), black_box(is_running.clone())))
+    });
+
+    let mut settings: AppSettings = AppSettings::new();
+    *settings.interval_milliseconds_mut() = 16;
+    *settings.click_type_mut() = MouseClickType::Double;
+    *settings.mouse_button_mut() = MouseButton::Left;
+    *settings.repeat_type_mut() = RepeatType::Repeat(10);
+    *settings.cursor_position_type_mut() = CursorPosition::FixedLocation(500, 500);
+
+    let settings = settings;
+
+    c.bench_function("double click, 10 times, 16ms", |b| {
         b.iter(|| worker_thread(black_box(settings.clone()), black_box(is_running.clone())))
     });
 }
